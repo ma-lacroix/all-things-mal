9d37c7e9268b7d165a05e540dc160c0e6497b90b
Marc-Antoine Lacroix <mlacroix@c02qc1a1fvh6.home>
2020-05-09 22:22:47 +0200

    Adding main file from section challenge in beginner c++ course

---
+++ cpp/menu_exercise/main.cpp
@@ -0,0 +1,133 @@
+// Section 9 - Challenge - Build a menu that can manage a list of integers
+// Author: Marc-Antoine Lacroix
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+char display_options() {
+// this function displays the menu options
+
+    char choice;
+    cout << " \nP - Print numbers" << endl;
+    cout << "A - Add a number" << endl;
+    cout << "M - Display mean of the numbers" << endl;
+    cout << "S - Display the smallest number" << endl;
+    cout << "L - Display the largest number" << endl;
+    cout << "Q - Quit \n" << endl;
+    cout << "\nPlease make your choice: ";
+    cin >> choice;
+
+    return choice;
+}
+
+int add_number(vector<int> &vec) {
+// this function adds a number to the list
+
+    int new_number = 0;
+    cout << "\nPlease enter a new number: ";
+    cin >> new_number;
+    vec.push_back(new_number);
+
+    return 0;
+}
+
+int print_numbers(vector<int> &vec) {
+// this function prints the list
+
+    int i = 0;
+    cout << "\n[ ";
+    while (i<vec.size()){
+        cout << vec.at(i) << " ";
+        i++;
+    }
+    cout << "]\n";
+
+    return 0;
+}
+
+int calc_average(vector<int> &vec) {
+// this function calculates the avg value of the list
+
+    int total = 0;
+    int length = vec.size();
+    int i = 0;
+    float avg = 0.0;
+    while (i<length){
+        total+=vec.at(i);
+        i++;
+    }
+    avg = static_cast<float>(total/length);
+    cout << "\nThe average number is: " << avg << endl;
+
+    return 0;
+
+}
+
+int smallest_num(vector<int> &vec) {
+// this function returns the smallest number of the list
+    
+    int smallest = vec.at(0);
+    int length = vec.size();
+    int i = 0;
+    while (i<length){
+        if (smallest > vec.at(i)) {
+            smallest = vec.at(i);
+        }
+        i++;
+    }
+    cout << "\nThe smallest number is: " << smallest << endl;
+    return 0;
+}
+
+int largest_num(vector<int> &vec) {
+// this function returns the highest number of the list
+    
+    int largest = vec.at(0);
+    int length = vec.size();
+    int i = 0;
+    while (i<length){
+        if (largest < vec.at(i)) {
+            largest = vec.at(i);
+        }
+        i++;
+    }
+    cout << "\nThe largest number is: " << largest << endl;
+    return 0;
+}
+
+int main() {
+    
+    vector<int> vec;
+    char selection;
+
+    cout << "\nWelcome! " << endl;
+
+    do {
+        selection = toupper(display_options());
+        cout << "\nYou have selected: " << selection << endl;
+        if (selection=='A') {
+            add_number(vec);
+        }
+        else if (selection=='P') {
+            print_numbers(vec);
+        }
+        else if (selection=='M') {
+            calc_average(vec);
+        }
+        else if (selection=='S') {
+            smallest_num(vec);
+        }
+        else if (selection=='L') {
+            largest_num(vec);
+        }
+        else {
+            cout << "\nPlease enter a valid choice." << endl;
+        }
+
+    } while (selection!='Q');
+        
+    return 0;
+}
+
