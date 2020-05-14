33c40befcab6512102b58cedf8a844d865fa0aa0
Marc-Antoine Lacroix <mlacroix@c02qc1a1fvh6.corpad.adbkng.com>
2020-05-14 23:21:38 +0200

    Finishing print string pyramid program

--- cpp/word_pyramid/pyramid_challenge.cpp
+++ cpp/word_pyramid/pyramid_challenge.cpp
@@ -3,34 +3,31 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 // +----------- Globals -----------+
 
+string message;
+string full_word;
+vector<string> mirror;
+
 // +----------- Functions -----------+
 
-// string enter_word() {
-//     string message;
-//     cout << "Enter a message: ";
-//     cin >> message;
-//     return message
-// }
-
-// string get_full_word(string message) {
-// }
-
-// string loop_mirror (string full_word) {
-// }
-
-int main () {
-// work in progress - testing basic string logic
-
+string enter_word() {
+// takes in a basic string message
     string message;
-    string inverted;
-
     cout << "Enter a message: ";
     cin >> message;
+    cout << "\n" << endl;
+    return message;
+}
+
+string get_full_word(string message) {
+// takes a string and mirrors it
+
+    string inverted;
     int mes_len = message.length()-1;
 
     for (size_t i = 0; i < mes_len ; ++i) {
@@ -39,18 +36,46 @@ int main () {
     }
     
     string full_word = message + inverted;
+    return full_word;
+}
+
+vector<string> loop_mirror (string full_word) {
+// creates a loop string vector
+
+    vector <string> mirror;
     int full_len = full_word.length()-1;
-
-    cout << full_word << endl;
-
+    mirror.push_back(full_word);
     for (size_t i = 0; full_len-(i*2) > 0; i++) {
-        
         full_word.replace(i,1," ");
         full_word.replace(full_len-i,1," ");
-        cout << full_word << endl;
+        mirror.push_back(full_word);
     }
+    return mirror;
+}
 
-    // cout << full_word.begin() << " " << full_word.end() << endl;
+string pyramid (vector<string> mirror){
+// prints out the pyramid-style message
+
+    int mirror_size = mirror.size();
+    
+    for (size_t i=1; mirror_size-i>0; ++i) {
+        int pos = mirror_size-i;
+        cout << mirror.at(pos) << endl;
+    }
+    cout << mirror.at(0) << "\n"<< endl;
+
+    return "";
+}
+
+int main () {
+// work in progress - testing basic string logic
+
+    message = enter_word();
+    full_word = get_full_word(message);
+    mirror = loop_mirror(full_word);
+    pyramid(mirror);
+
+    cout << "Program terminated" << endl;
 
     return 0;
 }
