a782f65608a85cd8c2b02af60e9362425099e68b
Marc-Antoine Lacroix <mlacroix@c02qc1a1fvh6.home>
2020-06-14 23:09:41 +0200

    Adding arrow keys input and coordinates updates

--- cPlusPlus/terminal_game/main.cpp
+++ cPlusPlus/terminal_game/main.cpp
@@ -6,49 +6,78 @@
 #include <thread>
 #include <chrono>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 using namespace std;
 
-string key_input() {
-    char ch;
-    system ("/bin/stty raw");
-    ch=getchar();
-        if(ch==65) {return "up";}
-        else if(ch==66) {return "down";}
-        else if(ch==67) {return "right";}
-        else if(ch==68) {return "left";}
-        else {return "nothing";}
-    system ("/bin/stty cooked");
-    }
-
-void adjust_position(vector<int> &position) {
-    string new_pos = key_input();
-    if (new_pos=="up") {position[0]-1;}
-    else if (new_pos=="down") {position[0]+1;}
-    else if (new_pos=="left") {position[1]-1;}
-    else if (new_pos=="right") {position[1]+1;}
+int keypress() {
+  system ("/bin/stty raw");
+  int c;
+  system("/bin/stty -echo");
+  c = getc(stdin);
+  system("/bin/stty echo");
+  system ("/bin/stty cooked");
+  return c;
 }
-
-vector<vector<string> > vect1{
-        {"#"},{"#"},{"#"}
-        };
 
 void refresh_screen() {
-    this_thread::sleep_until(chrono::system_clock::now() + 100000000ns);
+    // this_thread::sleep_until(chrono::system_clock::now() + 100000000ns);
+    this_thread::sleep_until(chrono::system_clock::now());
     system("clear");
 }
+
+string concat_char(vector<int> p, string &c) {
+// doesn't really work as expected - probably because of Unix terminal limitations
+    string vert,horiz;
+    for (int i=0;i<p[0];++i) {
+        vert+="\n";
+        horiz+=" ";
+    }
+    c=vert+horiz+c; // can't use += as we want the string to be at the end
+    return c;
+}
+
+void update_position(int key, vector<int> &position){
+    if (key==68) {
+        // cout << "left key" << endl;
+        position[0]-=1;
+    }
+    if (key==65) {
+        // cout << "up key" << endl;
+        position[1]+=1;
+    }
+    if (key==66) {
+        // cout << "down key" << endl;
+        position[1]-=1;
+    }
+    if (key==67) {
+        // cout << "right key" << endl;
+        position[0]+=1;
+    }
+}
+
 
 
 int main() {
     refresh_screen();
+    string dummy {"#"};
     vector<int> position {0,0};
-    adjust_position(position);
-    // while(1) {
-    //     for (size_t i=0;i<size(vect1);++i) {
-    //     cout << vect1[i][0] << endl;
-    //     }
-    // refresh_screen();
-    // }
+    int key;
+    
+    do {
+        refresh_screen();
+        // cout << concat_char(position,dummy);
+        cout << "\n\n x,y coord: " << position[0] << "," << position[1];
+        refresh_screen();
+        key = keypress();
+        update_position(key,position);
+        
+    } while (key!=113); // "q for quit"
+    
+    refresh_screen();
+
     return 0;
 }
 
