333e1d5a479ed7abcd7938238154633f1edb1417
Marc-Antoine Lacroix <mlacroix@c02qc1a1fvh6.home>
2020-06-15 23:20:11 +0200

    Adding code using ncurses library to Terminal game

--- cPlusPlus/terminal_game/main.cpp
+++ cPlusPlus/terminal_game/main.cpp
@@ -1,83 +1,69 @@
-// trying out building a very simple game engine in terminal
+// testing ncurses library
 
-#include <iostream> 
-#include <vector>
-#include <cstdlib>
-#include <thread>
-#include <chrono>
+#include <iostream>
 #include <string>
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <vector>
+#include <ncurses.h>
 
 using namespace std;
 
-int keypress() {
-  system ("/bin/stty raw");
-  int c;
-  system("/bin/stty -echo");
-  c = getc(stdin);
-  system("/bin/stty echo");
-  system ("/bin/stty cooked");
-  return c;
-}
-
-void refresh_screen() {
-    // this_thread::sleep_until(chrono::system_clock::now() + 100000000ns);
-    this_thread::sleep_until(chrono::system_clock::now());
-    system("clear");
-}
-
-string concat_char(vector<int> p, string &c) {
-// doesn't really work as expected - probably because of Unix terminal limitations
-    string vert,horiz;
-    for (int i=0;i<p[0];++i) {
-        vert+="\n";
-        horiz+=" ";
-    }
-    c=vert+horiz+c; // can't use += as we want the string to be at the end
-    return c;
-}
-
-void update_position(int key, vector<int> &position){
-    if (key==68) {
-        // cout << "left key" << endl;
-        position[0]-=1;
-    }
-    if (key==65) {
-        // cout << "up key" << endl;
-        position[1]+=1;
-    }
-    if (key==66) {
-        // cout << "down key" << endl;
-        position[1]-=1;
-    }
-    if (key==67) {
-        // cout << "right key" << endl;
-        position[0]+=1;
+void update_coordinates(int &y, int &x, int key) {
+    switch(key) {
+        case 65:
+            y-=1; 
+            break;
+        case 66:
+            // down key
+            y+=1; 
+            break;
+        case 67:
+            // right key
+            x+=1;
+            break; 
+        case 68:
+            // left key
+            x-=1;
+            break;
+        case 'q':
+            break;
     }
 }
 
+void init_ncurses() {
+/* ncurses initialisation stuff */
+    initscr(); 
+    noecho(); 
+    cbreak();
+    curs_set(0);
+}
 
-
-int main() {
-    refresh_screen();
-    string dummy {"#"};
-    vector<int> position {0,0};
+int main()
+{
     int key;
-    
-    do {
-        refresh_screen();
-        // cout << concat_char(position,dummy);
-        cout << "\n\n x,y coord: " << position[0] << "," << position[1];
-        refresh_screen();
-        key = keypress();
-        update_position(key,position);
-        
-    } while (key!=113); // "q for quit"
-    
-    refresh_screen();
+    int x {1};
+    int y {1};
+    char ch [2] {"#"};
+
+    // initializing stuff:
+    init_ncurses();
+    WINDOW* game_window = newwin(15,20,y,x);
+    refresh();
+    printw("Press any arrow key to start");
+    getch();
+    clear();
+
+    // game starts here: 
+    printw("This is supposed to be Tetris");
+    while((key=getch())!=113) { // 113 is code for "q" or "quit"
+        wclear(game_window);
+        wborder(game_window,(int)'|',(int)'|',(int)'-',(int)'-',(int)'+',(int)'+',(int)'+',(int)'+');
+        wmove(game_window,y,x);
+        wprintw(game_window,ch);
+        update_coordinates(y,x,key);
+        wrefresh(game_window);
+    }
+    // delwin(game_window);
+    endwin();                  
 
     return 0;
 }
-
