ccd98f84076825e218af0a505bde8a4b1819d88e
Marc-Antoine Lacroix <mlacroix@c02qc1a1fvh6.home>
2020-06-23 23:36:11 +0200

    Implenting Piece.h header file

--- cPlusPlus/terminal_game/main.cpp
+++ cPlusPlus/terminal_game/main.cpp
@@ -31,11 +31,14 @@ bool collision(vector<int> vec_y, vector<int> vec_x, int y, int x) {
     else {return false;}
 }   
 
-bool check_bottom(int size_y,int y, int x, vector<int> vec_y, vector<int> vec_x) {
+bool check_bottom(int size_y,Piece &p, int i, vector<int> vec_y, vector<int> vec_x) {
 // checks to see if the object has reached the bottom of the board or collides with another underneath
 // this function should be joined with another one
 
     int check {0};
+    int y {p.get_y_shape()[i]};
+    int x {p.get_x_shape()[i]};
+    
     for (size_t i=0;i<size(vec_y);++i) {
         if((y+1 == vec_y[i] && x == vec_x[i]) || y+1 == size_y-1) {
             check=1;
@@ -47,15 +50,18 @@ bool check_bottom(int size_y,int y, int x, vector<int> vec_y, vector<int> vec_x)
     else {return false;} 
 }
 
-void update_coordinates(vector<int> vec_y, vector<int> vec_x, int size_y, int size_x, int &y, int &x, int key) {
+void update_coordinates(vector<int> vec_y, vector<int> vec_x, int size_y, int size_x, Piece &p, int i, int key) {
 // updates the position of the character in the game window 
 // the if conditions insure you can't go overboard
 // if conditions will have to be updated as objects get bigger and have more coordinates
     
+    int y {p.get_y_shape()[i]};
+    int x {p.get_x_shape()[i]};
+
     switch(key) {
         case 65:
             // up key
-            y-=1;
+            p.update_y_shape(-1,i);
             // if (y>1 && !collision(vec_y,vec_x,y-1,x)) {
             //     y-=1;
             // }
@@ -63,16 +69,20 @@ void update_coordinates(vector<int> vec_y, vector<int> vec_x, int size_y, int si
         case 66:
             // down key
             if (y<size_y-2 && !collision(vec_y,vec_x,y+1,x)) {
-                y+=1;
+                p.update_y_shape(1,i);
             } 
             break;
         case 67:
             // right key
-            if (x<size_x-2 && !collision(vec_y,vec_x,y,x+1)) {x+=1;}
+            if (x<size_x-2 && !collision(vec_y,vec_x,y,x+1)) {
+                p.update_x_shape(1,i);
+            }
             break; 
         case 68:
             // left key
-            if (x>1 && !collision(vec_y,vec_x,y,x-1)) {x-=1;}
+            if (x>1 && !collision(vec_y,vec_x,y,x-1)) {
+                p.update_x_shape(-1,i);
+            }
             break;
         case 'q':
             break;
@@ -88,16 +98,13 @@ int main()
     int y_start {3};
     int x {x_start};
     int y {y_start};
-    // trying out a L-shaped object
-    // **** to be implemented in class "Piece" **** //
-    vector<int> y_init {y_start,y_start,y_start-1,y_start-2};
-    vector<int> x_init {x_start+1,x_start,x_start,x_start};
-    vector<int> y_shape {y,y,y-1,y-2};
-    vector<int> x_shape {x+1,x,x,x};
     char ch1 [2] {"#"};
     char ch2 [2] {"@"};
-
-    // Object b1(,size_x-2);
+    
+    // trying out a L-shaped object
+    Piece l_shape({y_start,y_start,y_start-1,y_start-2},
+                    {x_start+1,x_start,x_start,x_start},
+                    {y,y,y-1,y-2},{x+1,x,x,x});
 
     Objects blocks({size_y-2},{size_x-2}); // won't work if no blocks are pre-defined
 
@@ -109,33 +116,37 @@ int main()
     getch();
     clear();
 
-    // game loop: 
+    // // game loop: 
     printw("Tetris in the making\n");
 
     while((key=getch())!=113) { // 113 is code for "q" or "quit"
+        
         wclear(game_window);
-        // draw board
+        
+        /* draw board */
         wborder(game_window,(int)'|',(int)'|',(int)' ',(int)'-',(int)'+',(int)'+',(int)'+',(int)'+');
-        // cursor position
-        for (size_t i = 0;i<size(y_shape);++i){
-            wmove(game_window,y_shape[i],x_shape[i]);
+        
+        /* cursor position */
+        for (size_t i = 0;i<size(l_shape.get_y_shape());++i){
+            wmove(game_window,l_shape.get_y_shape()[i],l_shape.get_x_shape()[i]);
             wprintw(game_window,ch1); 
         }
-        //print static objects
+        /* print static objects */
         for (size_t i=0;i<size(blocks.get_y());++i) {
             wmove(game_window,blocks.get_y()[i],blocks.get_x()[i]);
             wprintw(game_window,ch2);
         }
-        for (size_t i = 0;i<size(y_shape);++i) {
-            update_coordinates(blocks.get_y(),blocks.get_x(),size_y,size_x,y_shape[i],x_shape[i],key);
+        for (size_t i = 0;i<size(l_shape.get_y_shape());++i) {
+            update_coordinates(blocks.get_y(),blocks.get_x(),size_y,size_x,l_shape,i,key);
         }
-        for (size_t i = 0;i<size(y_shape);++i) {
-            if (check_bottom(size_y,y_shape[i],x_shape[i],blocks.get_y(),blocks.get_x())) {
-                for (size_t i = 0;i<size(y_shape);++i) {
-                    blocks.push_x(x_shape[i]);
-                    blocks.push_y(y_shape[i]);
-                    x_shape[i]=x_init[i];
-                    y_shape[i]=y_init[i];
+        /* collision detection */
+        for (size_t i = 0;i<size(l_shape.get_y_shape());++i) {
+            if (check_bottom(size_y,l_shape,i,blocks.get_y(),blocks.get_x())) {
+                for (size_t i = 0;i<size(l_shape.get_y_shape());++i) {
+                    blocks.push_x(l_shape.get_x_shape()[i]);
+                    blocks.push_y(l_shape.get_y_shape()[i]);
+                    l_shape.update_x_shape(l_shape.get_x_init()[i],i);
+                    l_shape.update_y_shape(l_shape.get_y_init()[i],i);
                     wrefresh(game_window);
                 } 
             } else {wrefresh(game_window);}
--- cPlusPlus/terminal_game/piece.h
+++ cPlusPlus/terminal_game/piece.h
@@ -11,12 +11,12 @@
 class Piece {
 
 private:
+
     std::vector<int> y_init {};
     std::vector<int> x_init {};
     std::vector<int> y_shape {};
     std::vector<int> x_shape {};
-    char ch1 [2] {};
-    char ch2 [2] {};
+
 public:
     //constructors
     Piece();
@@ -24,25 +24,41 @@ public:
     Piece(const Piece &source);
     ~Piece();
     
+    //get functions
+    std::vector<int> get_y_init() {return y_init;};
+    std::vector<int> get_x_init() {return x_init;};
+    std::vector<int> get_y_shape() {return y_shape;};
+    std::vector<int> get_x_shape() {return x_shape;};
+
     //function prototypes
+    void update_y_shape(int y, int i);
+    void update_x_shape(int x, int i);
+
 };
 
 Piece::Piece() 
     :y_init{3,3,2,1},
     x_init{2,1,1,1},
     y_shape{3,3,2,1},
-    x_shape{2,1,1,1},
-    ch1{'#'},
-    ch2{'@'} {}
+    x_shape{2,1,1,1} {}
 
 Piece::Piece(std::vector<int> y_init,std::vector<int> x_init, 
             std::vector<int> y_shape,std::vector<int> x_shape)
     :y_init{y_init},
     x_init{x_init},
     y_shape{y_shape},
-    x_shape{x_shape},
-    ch1{'#'},
-    ch2{'@'} {}
+    x_shape{x_shape} {}
+
+Piece::Piece(const Piece &source)
+       : Piece {source.y_init, source.x_init, source.y_shape, source.x_shape}  {}
+
+void Piece::update_y_shape(int y, int i) {
+    y_shape[i] += y;
+}
+
+void Piece::update_x_shape(int x, int i) {
+    x_shape[i] += x;
+}
 
 Piece::~Piece() {}
 
