e769c33511ea850fe2486d87395071688a68cd76
ma-lacroix <lacroix.marcantoine@gmail.com>
2020-10-11 12:17:26 +0200

    Grab branch logic

--- cPlusPlus/tree_jump/app.h
+++ cPlusPlus/tree_jump/app.h
@@ -28,7 +28,7 @@ void app::game(sf::RenderWindow& window,sf::View view,sf::View HUD,float VIEW_HE
     // Tree branches
     Branch* rect1 = new Branch({200.0f,50.0f},{100.0f,150.0f});
     Branch* rect2 = new Branch({300.0f,50.0f},{300.0f,300.0f});
-    Branch* rect3 = new Branch({200.0f,50.0f},{150.0f,550.0f});
+    Branch* rect3 = new Branch({200.0f,50.0f},{150.0f,600.0f});
     Branch* rect4 = new Branch({200.0f,50.0f},{250.0f,800.0f});
     Branch* rect5 = new Branch({200.0f,50.0f},{90.0f,1000.0f});
     std::vector<Branch*> rects;
@@ -49,14 +49,18 @@ void app::game(sf::RenderWindow& window,sf::View view,sf::View HUD,float VIEW_HE
     trunks.push_back(trunk3);
     trunks.push_back(trunk4);
 
-    // main player
-    Player player({100.0f,100.0f},{300.0f,1100.0f});
+    // main player & its arms
+    Player* player_arm = new Player({30.0f,60.0f},{300.0f,1100.0f},300.0f,true);
+    Player* main_player = new Player({100.0f,100.0f},{300.0f,1100.0f},100.0f,false);
+    std::vector<Player*> players;
+    players.push_back(player_arm);
+    players.push_back(main_player);
 
     // game loop
     while (window.isOpen()){
 
         deltaTime = clock.restart().asSeconds();
-        view.setCenter(VIEW_HEIGHT/2,player.getPosition().y-100.0f);
+        view.setCenter(VIEW_HEIGHT/2,main_player->getPosition().y-100.0f);
         
         
         // this part handles events related to the actual game window like closing or resizing. 
@@ -82,13 +86,15 @@ void app::game(sf::RenderWindow& window,sf::View view,sf::View HUD,float VIEW_HE
             }
         }
         
-        // handle objects
-
+        // handle & draw objects
         window.clear(sf::Color(245,230,230));
         window.setView(view);
         Ycoord = view.getCenter().y;
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-            player.setMouseClickPos(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
+            for (auto plr: players){
+                plr->setMouseClickPos(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
+                plr->Rotate();
+            }
         }
         
         for (auto trunk: trunks){
@@ -98,22 +104,25 @@ void app::game(sf::RenderWindow& window,sf::View view,sf::View HUD,float VIEW_HE
 
         for (auto rect: rects){
             rect->updateYcoord(Ycoord);
-            rect->updateColor(rect->Collision(player.getClickPos(),player.getVelocity().y));
+            rect->updateColor(rect->Collision(main_player->getClickPos(),main_player->getVelocity().y));
             rect->Draw(window);
         }
         
+        
         for (auto rect: rects){
-            if(rect->Collision(player.getClickPos(),player.getVelocity().y)){
-                player.updateState('M');
-                player.State(deltaTime);
-                break;
-            }else{
-                player.updateState('S');
-                player.State(deltaTime);
+            for (auto plr: players){
+                if(rect->Collision(plr->getClickPos(),plr->getVelocity().y)){
+                    plr->updateState('M');
+                    plr->State(deltaTime);
+                    plr->Draw(window);
+                    break;
+                }else{
+                    plr->updateState('S');
+                    plr->State(deltaTime);
+                    plr->Draw(window);
+                }
             }
         }
-        
-        player.Draw(window);
         window.display();
     }
 }
--- cPlusPlus/tree_jump/player.h
+++ cPlusPlus/tree_jump/player.h
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <math.h>
 #include "object.h"
+#define PI 3.14159265
 
 class Player: public Object {
 private:
@@ -14,12 +15,14 @@ private:
     sf::Vector2f velocity;
     sf::Vector2f mouseClickPos;
     float speed;
+    bool arm;
     char playstate;
 public:
     // Player(sf::Texture* textureFile,sf::Vector2f PlayerSize, sf::Vector2f position, bool clickable);
-    Player(sf::Vector2f, sf::Vector2f);
+    Player(sf::Vector2f,sf::Vector2f,float,bool);
     ~Player();
     void State(float);
+    void Rotate();
     void Movement(float);
     void Stop();
     void Draw(sf::RenderWindow&);
@@ -31,14 +34,15 @@ public:
     char getPlaystate() {return playstate;};
 };
 
-Player::Player(sf::Vector2f objectSize, sf::Vector2f position)
+Player::Player(sf::Vector2f objectSize, sf::Vector2f position, float speed, bool arm)
     :Object(objectSize,position){
     shape.setPosition(position);
     shape.setSize(objectSize);
+    this->speed = speed;
+    this->arm = arm;
     // shape.setOrigin(400.0f,400.0f);
     velocity = {0.0f,0.0f};
     mouseClickPos = {0.0f,0.0f};
-    speed = 150.0f;
     playstate = 'S';
 }
 
@@ -48,14 +52,24 @@ Player::~Player(){
 void Player::State(float deltatime){
     switch (playstate){
     case 'M':
-        Movement(deltatime);
         shape.setFillColor(sf::Color::Cyan);
+        Movement(deltatime);
         break;
     case 'S':
         Stop();
-        shape.setFillColor(sf::Color::Blue);
     default:
         break;
+    }
+}
+
+void Player::Rotate(){
+    // Pythagoras theorem to determine angle of shape
+    if(arm){
+        float a = mouseClickPos.y-shape.getPosition().y;
+        float b = mouseClickPos.x-shape.getPosition().x;
+        float angle = atanf(a/b)*180/PI;
+        printf("angle: %f\n",angle);
+        shape.setRotation(angle);
     }
 }
 
@@ -64,7 +78,7 @@ void Player::Movement(float deltatime){
     sf::Vector2f direction_norm;
     direction.x = mouseClickPos.x-(shape.getPosition().x+shape.getSize().x/2);
     direction.y = mouseClickPos.y-(shape.getPosition().y+shape.getSize().y);
-    direction_norm = direction / sqrtf(direction.x * direction.x + direction.y * direction.y);
+    direction_norm = direction / sqrtf(direction.x * direction.x + direction.y * direction.y); // avoid speed changes
     velocity = direction_norm*deltatime*speed;
     if(abs(direction.x+direction.y)>0.7f){
         shape.move(velocity);
