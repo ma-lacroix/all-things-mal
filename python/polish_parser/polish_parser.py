5c2b514d50028250e2f8e3253d50f9f57a11995c
ma-lacroix <lacroix.marcantoine@gmail.com>
2020-11-15 23:18:03 +0100

    Last solution to problem

--- python/polish_parser/polish_parser.py
+++ python/polish_parser/polish_parser.py
@@ -2,23 +2,33 @@
 # Author: Marc-Antoine Lacroix
 # WIP
 
-def eval(obj,alist):
-    x=0
-    for element in obj:
-        if(type(element) is list):
-            eval(element,alist)
-        else:
-            x=element
-            alist.append(x)
-            print(x)
-    return sum(alist)            
+def getSymbol(list1,list2):
+    if(list1[0]=='-'):
+        res = list2[0]-list2[1]
+    elif(list1[0]=='+'):
+        res = list2[0]+list2[1]
+    elif(list1[0]=='/'):
+        res = list2[0]/list2[1]
+    elif(list1[0]=='*'):
+        res = list2[0]*list2[1]
+    return res
 
+def eval(obj):
+    alist = []
+    if(type(obj[1]) is list):
+        alist.append(eval(obj[1]))
+    else:
+        alist.append(obj[1])
+    if(type(obj[2]) is list):
+        alist.append(eval(obj[2]))
+    else:
+        alist.append(obj[2])
+    return getSymbol(obj,alist)
+    
 def main():
-    # obj = ['+',['-',4,3],1]
-    obj = [[1,2,3],[4,5,[6,7,8]]]
-    l = []
-    x = eval(obj,l)
-    print(x)
+    obj = ['-',['+',['-',['/',8,2],3],7],['+',2,['-',['*',2,2],1]]]
+    res = eval(obj)
+    print("Final result: {}".format(res))
     
 if __name__ == "__main__":
     main()
