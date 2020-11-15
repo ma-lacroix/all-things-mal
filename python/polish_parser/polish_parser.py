41b0f2f69564ef9ce03c0223085aeec944ca281f
ma-lacroix <lacroix.marcantoine@gmail.com>
2020-11-15 21:46:51 +0100

    Working prototype

--- python/polish_parser/polish_parser.py
+++ python/polish_parser/polish_parser.py
@@ -2,18 +2,23 @@
 # Author: Marc-Antoine Lacroix
 # WIP
 
-def eval(obj,i):
-    olen = len(obj)
-    if(i>olen):
-        return
-    eval(obj,i+1)
-    print(obj)
+def eval(obj,alist):
+    x=0
+    for element in obj:
+        if(type(element) is list):
+            eval(element,alist)
+        else:
+            x=element
+            alist.append(x)
+            print(x)
+    return sum(alist)            
 
 def main():
     # obj = ['+',['-',4,3],1]
-    obj = [[1,2,3],[4,5,6],[7,8,[9,10,11]]]
-    i = 0
-    eval(obj,i)
+    obj = [[1,2,3],[4,5,[6,7,8]]]
+    l = []
+    x = eval(obj,l)
+    print(x)
     
 if __name__ == "__main__":
     main()
