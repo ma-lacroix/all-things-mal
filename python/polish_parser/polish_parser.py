a27dc7de6874865e399831b704efb2fe33954ab6
ma-lacroix <lacroix.marcantoine@gmail.com>
2020-11-15 23:30:30 +0100

    Minor code refactoring

--- python/polish_parser/polish_parser.py
+++ python/polish_parser/polish_parser.py
@@ -2,15 +2,15 @@
 # Author: Marc-Antoine Lacroix
 # WIP
 
-def getSymbol(list1,list2):
-    if(list1[0]=='-'):
-        res = list2[0]-list2[1]
-    elif(list1[0]=='+'):
-        res = list2[0]+list2[1]
-    elif(list1[0]=='/'):
-        res = list2[0]/list2[1]
-    elif(list1[0]=='*'):
-        res = list2[0]*list2[1]
+def useSymbol(symbol,alist):
+    if(symbol=='-'):
+        res = alist[0]-alist[1]
+    elif(symbol=='+'):
+        res = alist[0]+alist[1]
+    elif(symbol=='/'):
+        res = alist[0]/alist[1]
+    elif(symbol=='*'):
+        res = alist[0]*alist[1]
     return res
 
 def eval(obj):
@@ -23,7 +23,7 @@ def eval(obj):
         alist.append(eval(obj[2]))
     else:
         alist.append(obj[2])
-    return getSymbol(obj,alist)
+    return useSymbol(obj[0],alist)
     
 def main():
     obj = ['-',['+',['-',['/',8,2],3],7],['+',2,['-',['*',2,2],1]]]
