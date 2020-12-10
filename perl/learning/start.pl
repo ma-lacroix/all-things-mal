5f167a761bf539fd02910f1608e813701df9a143
ma-lacroix <lacroix.marcantoine@gmail.com>
2020-12-10 21:07:26 +0100

    More learning Perl code

--- perl/learning/start.pl
+++ perl/learning/start.pl
@@ -4,8 +4,8 @@
 use strict; 
 use warnings;
 
-sub main() {
-    
+sub loop() {
+# Loop example
     print("\nProgram is starting\n");
 
     my @files = ('/Users/mlacroix/Documents/demo.mov',
@@ -21,6 +21,27 @@ sub main() {
         }
     }
 
+}
+
+sub readStuff(){
+# Reading stuff from a file and insert it into another
+    my $textFile = 'aText.txt';
+    my $newFile = 'outputText.txt';
+    open(INPUT,'<',$textFile);
+    open(OUTPUT,'>',$newFile);
+    while(my $line = <INPUT>){
+        if($line =~ /^I/){ # REGEXP example
+            print(OUTPUT "$line\n");
+        }
+    }
+    close(INPUT);
+    close(OUTPUT);
+
+    die "Input file $textFile killed\n";
+}
+
+sub main(){
+    readStuff();
 }
 
 main();
