5e734a4cd7b475cc6ea3b70d8c59046060b3591c
ma-lacroix <lacroix.marcantoine@gmail.com>
2021-01-24 23:45:29 +0100

    Sharpe ratio calculations

--- python/bid_bot/main.py
+++ python/bid_bot/main.py
@@ -2,7 +2,8 @@
 # testing some APIs and getting a hang of things
 
 import datetime
-import pandas
+import pandas as pd
+import numpy as np
 from tradingview_ta import TA_Handler, Interval
 import pandas_datareader.data as web
 
@@ -42,31 +43,62 @@ def get_close_prices(timeframe,security):
     service = 'yahoo'
     end = datetime.datetime.today().strftime('%Y-%m-%d')    
     if(timeframe=='1d'):
-        start = (datetime.datetime.today()-datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+        start = (datetime.datetime.today()-datetime.timedelta(days=2)).strftime('%Y-%m-%d')
     elif(timeframe=='1w'):
-        start = (datetime.datetime.today()-datetime.timedelta(days=7)).strftime('%Y-%m-%d')
+        start = (datetime.datetime.today()-datetime.timedelta(days=8)).strftime('%Y-%m-%d')
     elif(timeframe=='1m'):
-        start = (datetime.datetime.today()-datetime.timedelta(days=30)).strftime('%Y-%m-%d')
+        start = (datetime.datetime.today()-datetime.timedelta(days=31)).strftime('%Y-%m-%d')
+    elif(timeframe=='1y'):
+        start = (datetime.datetime.today()-datetime.timedelta(days=365)).strftime('%Y-%m-%d')
     else:
         start = end
     try:
         df = web.DataReader(security,service,start,end)[['Close','Volume']]
+        df.sort_values('Date',inplace = True)
     except ValueError as error:
         print("Couldn't connect to {} - {}".format(service,error))
     return df
 
+def get_sharp_ratio(simulations,df):
+    print("\nGetting optimal portfolio distribution\n{}".format(list(df.columns)))
+    all_weights = np.zeros((simulations,len(df.columns)))
+    ret_arr = np.zeros(simulations)
+    vol_arr = np.zeros(simulations)
+    sharpe_arr = np.zeros(simulations)
+
+    for ind in range(simulations):
+        weights = np.array(np.random.random(len(df.columns)))
+        weights = weights / np.sum(weights)
+        all_weights[ind,:] = weights
+        ret_arr[ind] = np.sum((df.mean() * weights) *len(df))
+        vol_arr[ind] = np.sqrt(np.dot(weights.T, np.dot(df.cov() * len(df), weights)))
+        sharpe_arr[ind] = ret_arr[ind]/vol_arr[ind]
+    return all_weights[sharpe_arr.argmax(),:]
+
+def get_log_ret(intdict,securities):
+    symb_list = []
+    for item in securities:
+        symb_list.append(securities[item][1][0])
+    df = get_close_prices(intdict,symb_list)
+    log_ret = pd.DataFrame(np.log(df['Close']/df['Close'].shift(1)))
+    log_ret.fillna(0,inplace=True)
+    return log_ret
+
 def print_results(intdict,securities):
     print("\nToday's date: {}\r".format(datetime.date.today()))
-    for item in securities:
-        print("\n{}\n".format(securities[item][1][0]))
-        for intd in intdict:
-            df = get_close_prices(intd,securities[item][1][0])
+    for intd in intdict:
+        for item in securities:    
+            print("\n{}".format(securities[item][1][0]))
             try:
                 recom = get_recommendation(securities[item][0],securities[item][1],intdict[intd][0])
+                print("Based on {} -> {}".format(intdict[intd][1],recom))
             except ValueError as error:
                 print("Couldn't connect to TradingView - {}".format(error))
-            print("Based on {} -> {}\n".format(intdict[intd][1],recom))
-            print(df)
+        try:
+            sol = get_sharp_ratio(15000,get_log_ret(intd,securities))
+            print("Optimal portfolio allocation: {}".format(sol))
+        except ValueError as err:
+            print("Couldn't get Sharpe ratios - {}".format(err))
         print("\r")
 
 def main():
