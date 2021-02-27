0e105b1e5ac5cd34ce2292b4c6c42bd0bcd6ba93
ma-lacroix <lacroix.marcantoine@gmail.com>
2021-02-27 12:53:14 +0100

    Adding folder for new c++ course

---
+++ c++/learning/adv_cpp_course/exception.cpp
@@ -0,0 +1,33 @@
+// exception handling and error example 1
+
+#include <iostream>
+#include <string>
+
+void somethingGoingWrong(bool error1, bool error2){
+
+    if(error1){
+        throw 10;
+    }
+    if(error2){
+        throw std::string("This is wrong dude.");
+    }
+}
+
+void checkingProcess(bool error1, bool error2){
+    somethingGoingWrong(error1,error2);
+}
+
+int main() {
+
+    try{
+        somethingGoingWrong(false,true);
+    }
+    catch(int err1){
+        std::cout << "Error code: " << err1 << std::endl;
+    }
+    catch(std::string &err2){
+        std::cout << err2 << std::endl;
+    }
+    
+    return 0;
+}
