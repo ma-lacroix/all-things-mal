1af6a687f48f71bc51bbf08e8fcd87822e036a49
ma-lacroix <lacroix.marcantoine@gmail.com>
2021-03-01 12:34:18 +0100

    Notes on lists

---
+++ c++/learning/adv_cpp_course/funwithlists.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <list>
+
+int main(){
+    
+    std::list<int> testing {10,40,40};
+    testing.push_front(5);
+
+    std::list<int>::iterator it = testing.begin();
+    ++it;
+    testing.insert(it,32);
+
+    for(it = testing.begin();it != testing.end();++it){
+        std::cout << *it << " ";
+    }
+
+    return 0;
+}
