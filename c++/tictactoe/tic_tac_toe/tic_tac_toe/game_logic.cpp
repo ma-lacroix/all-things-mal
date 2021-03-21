2ede9a1c19812b16a1e7538c33f16eabe76214f5
ma-lacroix <mlacroix@c02x71nzjhd4.home>
2021-03-21 22:55:28 +0100

    building tictactoe game with Xcode

---
+++ c++/tictactoe/tic_tac_toe/tic_tac_toe/game_logic.cpp
@@ -0,0 +1,9 @@
+//
+//  game_logic.cpp
+//  tic_tac_toe
+//
+//  Created by Marc-Antoine Lacroix on 21/03/2021.
+//  Copyright © 2021 Marc-Antoine Lacroix. All rights reserved.
+//
+
+#include "game_logic.hpp"
