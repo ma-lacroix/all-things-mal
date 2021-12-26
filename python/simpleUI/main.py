fa28618ce771d66ffa1bb91fc28b8696e5734b29
ma-lacroix <lacroix.marcantoine@gmail.com>
2021-12-26 12:40:27 +0100

    First tk class

--- python/simpleUI/main.py
+++ python/simpleUI/main.py
@@ -1,18 +1,28 @@
 # Trying out tkinter class
 
 import time
-import tkinter
+import tkinter as tk
 
-class someClass:
+class someWindow:
+    
     def __init__(self,name):
         self.name = name
-
+        self.window = tk.Tk()
+        self.window.title(self.name)
+        self.window.geometry('600x400+50+50')
+        self.top = tk.Label(self.window,text="first line")
+    
     def getName(self):
         return self.name
 
+    def startLoop(self):
+        self.top.pack()
+        self.window.mainloop()
+
 def main():
-    marc = someClass("M-A")
-    print(f"Hello world {marc.getName()}")
+    newWindow = someWindow("First TK project")
+    print(f"Starting program {newWindow.getName()}")
+    newWindow.startLoop()
 
 if __name__=="__main__":
     main()
