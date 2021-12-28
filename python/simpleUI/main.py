c5af73d6c47b9b4de12282bbfddc54bcb91a2209
ma-lacroix <lacroix.marcantoine@gmail.com>
2021-12-28 20:53:20 +0100

    buttons

--- python/simpleUI/main.py
+++ python/simpleUI/main.py
@@ -1,64 +1,44 @@
 # Trying out tkinter class
 
-import time
 import tkinter as tk
-from tkinter import Button, ttk
-from typing import Sized
+from tkinter import ttk
+
+class createButtons:
+    def __init__(self,name,col,row) -> None:
+        self.button = ttk.Button(text=name)
+        self.col = col
+        self.row = row
+    
+    def addToBoard(self):
+        self.button.grid(column=self.col,row=self.row)
 
 class mainWindow(tk.Tk):
     
     def __init__(self,name):
         super().__init__()
+        self.style = ttk.Style(self)
+        self.style.configure('TButton', font=('Helvetica', 10))
         self.name = name
         self.geometry('300x300+50+50')
         self.title(self.name)
-        self.columnconfigure(0,weight=1)
-        self.columnconfigure(1,weight=1)
-        self.columnconfigure(2,weight=1)
-        self.columnconfigure(3,weight=1)
-        self.rowconfigure(0,weight=1)
-        self.rowconfigure(1,weight=1)
-        self.rowconfigure(2,weight=1)
-        self.rowconfigure(3,weight=1)
         self.setUpBoard()
-        
-        # self.line1 = tk.Label(self.window,text="first line")
-        # self.line2 = tk.Label(self.window,text="second line")
     
     def getName(self):
         return self.name
 
     def setUpBoard(self):
-        b_one = Button(self, text="1",width=5,height=5,highlightbackground='blue',highlightcolor='red')
-        b_one.grid(column=0, row=0)
-        b_two = Button(self, text="2",width=5,height=5)
-        b_two.grid(column=1, row=0)
-        b_three = Button(self, text="3",width=5,height=5)
-        b_three.grid(column=2, row=0)
-        b_four = Button(self, text="4",width=5,height=5)
-        b_four.grid(column=0, row=1)
-        b_five = Button(self, text="5",width=5,height=5)
-        b_five.grid(column=1, row=1)
-        b_six = Button(self, text="6",width=5,height=5)
-        b_six.grid(column=2, row=1)
-        b_seven = Button(self, text="7",width=5,height=5)
-        b_seven.grid(column=0, row=2)
-        b_eight = Button(self, text="8",width=5,height=5)
-        b_eight.grid(column=1, row=2)
-        b_nine = Button(self, text="9",width=5,height=5)
-        b_nine.grid(column=2, row=2)
-        b_plus = Button(self,text="+",width=5,height=5)
-        b_plus.grid(column=3,row=0)
-        b_minus = Button(self,text="-",width=5,height=5)
-        b_minus.grid(column=3,row=1)
-        b_divide = Button(self,text="/",width=5,height=5)
-        b_divide.grid(column=3,row=2)
-        b_multiply = Button(self,text="*",width=5,height=5)
-        b_multiply.grid(column=3,row=3)
-
-        # button
-        button = Button(self, text="Run!",width=10,height=5)
-        button.grid(column=1, row=3)
+        button_names= ["\n1\n","\n2\n","\n3\n",
+                        "\n4\n","\n5\n","\n6\n",
+                        "\n7\n","\n8\n","\n9\n",
+                        "\n0\n"]
+        i,j = 0,0
+        for button in button_names:
+            createButtons(button,i,j).addToBoard()
+            if i%2==0 and i>0:
+                i=0
+                j+=1
+            else:
+                i+=1
 
 def main():
     newWindow = mainWindow("Calculator")
