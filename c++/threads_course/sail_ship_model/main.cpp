ab74b39b9cf3f7f7ce5cecb6db2f50cf2f638b5e
ma-lacroix <lacroix.marcantoine@gmail.com>
2022-05-31 13:33:07 +0200

    finished ex2 section 1

--- c++/threads_course/sail_ship_model/main.cpp
+++ c++/threads_course/sail_ship_model/main.cpp
@@ -1,97 +1,118 @@
+#include <chrono>
+#include <queue>
 #include <thread>
 #include <iostream>
 
 class Cleaners {
+
 public:
     Cleaners() {};
-    void clean() {
-        std::cout << "cleaning ship..." << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-        std::cout << "cleaning ship done!" << std::endl;
+
+    void cleaningCrewActivity(std::queue<int>& tasks) {
+        while (true) {
+            std::cout << "Number of tasks: " << tasks.size() << std::endl;
+            if (tasks.empty()) {
+                std::cout << " Cleaning Crew - Activity Queue is empty: Waiting for 2 seconds\n";
+                std::this_thread::sleep_for(std::chrono::seconds(3));
+            } else {
+                int currentActivity = tasks.front();
+                tasks.pop();
+                if (currentActivity == 100) {
+                    std::cout << " Cleaning Crew - All Activities Completed - Exiting\n";
+                    break;
+                } else {
+                    std::cout << " Cleaning Crew - Cleaning \n";
+                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                    std::cout << " Cleaning Crew - Cleaning DONE \n";
+                    std::cout << " Cleaning Crew is resting for 1 seconds after tiresome work \n";
+                    std::this_thread::sleep_for(std::chrono::seconds(1));
+                }
+            }
+        }
     }
+
     ~Cleaners() {};
 };
 
 class EngineCrew {
+
+public:
+    std::queue<int> tasks;
+
 public:
     EngineCrew() {};
-    void move() {
-        std::cout << "moving ship..." << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-        // TODO: does not print! 
-        std::cout << "moving ship done!" << std::endl;
-    }
-    void stop() {
-        std::cout << "stopping ship..." << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(6000));
-        // TODO: does not print!
-        std::cout << "stopping ship done!" << std::endl;
-    }
+    
+    void engineCrewActivity(std::queue<int>& tasks) {
+
+	while (true) {
+		if (tasks.empty()) {
+            std::cout << "Number of tasks: " << tasks.size() << std::endl;
+			std::cout << " Engine Crew - Activity Queue is empty: Waiting for 2 seconds\n";
+			std::this_thread::sleep_for(std::chrono::seconds(3));
+		} else {
+			int currentActivity = tasks.front();
+			tasks.pop();
+			if (currentActivity == 100) {
+				std::cout << " Engine Crew - All Activities Completed - Exiting\n";
+				break;
+			} else if (currentActivity == 2) {
+				std::cout << " Engine Crew - Full Speed Ahead \n";
+				std::this_thread::sleep_for(std::chrono::milliseconds(500));
+				std::cout << " Engine Crew - Full Speed Ahead DONE \n";
+ 
+			} else {
+				std::cout << " Engine Crew - Full Speed Ahead \n";
+				std::this_thread::sleep_for(std::chrono::milliseconds(500));
+				std::cout << " Engine Crew - Full Speed Ahead DONE \n";
+			}
+			std::cout << " Engine Crew is resting for 1 seconds after tiresome work \n";
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+	}
+}
+
     ~EngineCrew() {};
-};
-
-class Captain {
-
-public:
-    Cleaners cleaners;
-    EngineCrew engineCrew;
-    Captain(Cleaners *cleaners, EngineCrew *engineCrew) {
-        this->cleaners = *cleaners;
-        this->engineCrew = *engineCrew;
-    }
-    void move_forward() {
-        std::cout << "Captain says: move forward" << std::endl;
-        engineCrew.move();
-    }
-    void stop_moving() {
-        std::cout << "Captain says: stop moving" << std::endl;
-        engineCrew.stop();
-    }
-    void clean_ship() {
-        std::cout << "Captain says: clean ship" << std::endl;
-        cleaners.clean();
-    }
-    ~Captain() {
-        cleaners.~Cleaners();
-        engineCrew.~EngineCrew();
-    };
 };
 
 int main() {
 
     std::cout << "Starting program" << std::endl;
-    
+
     Cleaners *cleaners = new Cleaners();
     EngineCrew *engineCrew = new EngineCrew();
-    Captain *captain = new Captain(cleaners, engineCrew);
     
     int user_input;
+    std::queue<int> cleaningQueue, engineQueue;
+    std::thread cleanerCrew_thread(&Cleaners::cleaningCrewActivity, *cleaners, std::ref(cleaningQueue));
+    std::thread engineCrew_thread(&EngineCrew::engineCrewActivity, *engineCrew, std::ref(engineQueue));
 
-    while (user_input != 100) {
-        
-        std::cout << "\n1: clean the ship! \n" 
+    std::string menu = "\n1: clean the ship! \n" 
         "2: move the ship forward! \n"
         "3: stop the ship!\n" 
-        "100: quit\n" << std::endl;
+        "100: quit\n";
+
+    while (true) {
+        
+        std::cout << menu;
         std::cout << "Enter the captain's order: " << std::endl;
         std::cin >> user_input;
 
-        if (user_input == 1) {
-            std::thread thread1(&Captain::clean_ship, *captain);
-            thread1.detach();
+        if (user_input == 1) {     
+            cleaningQueue.push(1);            
         } else if (user_input == 2) {
-            std::thread thread2(&Captain::move_forward, *captain);
-            thread2.join();
+            engineQueue.push(2);
         } else if (user_input == 3){
-            std::thread thread3(&Captain::stop_moving, *captain);
-            thread3.join();
+            engineQueue.push(3);
         } else if (user_input == 100) {
             std::cout << "\nDone!\n" << std::endl;
+            cleaningQueue.push(100);
+            engineQueue.push(100);
+            break;
         } else {
             std::cout << "Invalid input" << std::endl;
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(50)); // for printing purposes
     }
-
+    cleanerCrew_thread.join();
+    engineCrew_thread.join();
     return 0;
 }
